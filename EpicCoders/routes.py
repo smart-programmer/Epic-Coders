2cf3e10db5224299caa4944cda65fc986c8ba171
Ammar.Programmer <gbeast123488@gmail.com>
2018-11-03 20:08:48 +0300

    trying to fix syuff

--- EpicCoders/routes.py
+++ EpicCoders/routes.py
@@ -13,7 +13,7 @@ import random
 
 @app.route('/')
 def Home(): 
-	courses = Course.query.filter_by(course_accessibility='public')
+	courses = Course.query.filter_by(course_accessibility='public').all()
 	return render_template("Home.html", is_Home=True, without_background=True, courses=courses)
 
 
