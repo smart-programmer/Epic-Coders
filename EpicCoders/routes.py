2fed426b2f340ef77580e6605a3fa8ef92d343d5
Ammar.Programmer <gbeast123488@gmail.com>
2018-11-03 19:09:41 +0300

    trying to fix limit2

--- EpicCoders/routes.py
+++ EpicCoders/routes.py
@@ -13,7 +13,8 @@ import random
 
 @app.route('/')
 def Home(): 
-	courses = Course.query.filter_by(course_accessibility='public')[:4]
+	courses = Course.query.filter_by(course_accessibility='public')
+	courses = courses[:4]
 	return render_template("Home.html", is_Home=True, without_background=True, courses=courses)
 
 
