101b514a7373344d7f0d11607842a09af580f56d
Ammar.Programmer <gbeast123488@gmail.com>
2018-11-05 06:39:15 +0300

    changing to heroku database

--- EpicCoders/__init__.py
+++ EpicCoders/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'c9305a6d0899d06a9352c31adf75f8478b'
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../sqlalchemy.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://vvvsdeqbcwyqnw:e92ddeb85c124176843e713e5766ab3bfd0f4bf8fff44f372982d98dea0fb588@ec2-54-243-187-30.compute-1.amazonaws.com:5432/dbnq788gc67rbo"
 
 db = SQLAlchemy(app)
 
